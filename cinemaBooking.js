0f11d962080b63356694be40b4940b0a32e89295
metamiaip <metamia@gmail.com>
2021-11-13 23:06:11 +0000

    add function to check number of seats left in the cinema and add test case

--- cinemaBooking.js
+++ cinemaBooking.js
@@ -2,12 +2,36 @@ function getRandomSeat (min, max) {
     return Math.round(Math.random() * (max - min)) + min;
 }
 
-function test () {
-    return true;
+const cinemaSeats = [
+    {SeatNo:"A1", Booked:"N"},
+    {SeatNo:"A2", Booked:"N"},
+    {SeatNo:"A3", Booked:"N"},
+    {SeatNo:"A4", Booked:"N"},
+    {SeatNo:"A5", Booked:"N"},
+    {SeatNo:"B1", Booked:"N"},
+    {SeatNo:"B2", Booked:"N"},
+    {SeatNo:"B3", Booked:"N"},
+    {SeatNo:"B4", Booked:"N"},
+    {SeatNo:"B5", Booked:"N"},
+    {SeatNo:"C1", Booked:"N"},
+    {SeatNo:"C2", Booked:"N"},
+    {SeatNo:"C3", Booked:"N"},
+    {SeatNo:"C4", Booked:"N"},
+    {SeatNo:"C5", Booked:"N"}
+];
+
+function chkNumOfSeatsLeft() {
+    let cnt = 0;
+    for (let i=0; i<cinemaSeats.length; i++) {
+        if (cinemaSeats[i].Booked=="N") 
+            cnt++;
+    }
+    return cnt;
 }
 
 module.exports = {
-    getRandomSeat
+    getRandomSeat,
+    chkNumOfSeatsLeft
 };
 
 //console.log(getRandomSeat(1,3));
--- cinemaBooking.test.js
+++ cinemaBooking.test.js
@@ -1,5 +1,6 @@
 const {
-    getRandomSeat
+    getRandomSeat,
+    chkNumOfSeatsLeft
 } = require('./cinemaBooking');  
 
 describe("test get the random seat number between 1 and 3", () => {
@@ -11,4 +12,10 @@ describe("test get the random seat number between 1 and 3", () => {
         expect(getRandomSeat(1,3)).toBeGreaterThanOrEqual(1);
       });   
       
+});
+
+describe("check number of seats left in the cinema", () => {
+  test("return 15 seats", () => {
+    expect(chkNumOfSeatsLeft()).toBe(15);
+  });     
 });
